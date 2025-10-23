2fc61e44dc865b850e472e9bd6736ce68f7d72b8
Vinícius Arruda <arrudavini25@gmail.com>
2025-10-22 22:40:12 -0300

    feat/create the missing controllers of environment and controller entities

---
+++ src/main/java/br/edu/ifpe/sigma/sigma/controller/ComponentController.java
@@ -0,0 +1,49 @@
+package br.edu.ifpe.sigma.sigma.controller;
+
+import br.edu.ifpe.sigma.sigma.dto.component.ComponentRequestDTO;
+import br.edu.ifpe.sigma.sigma.dto.component.ComponentResponseDTO;
+import br.edu.ifpe.sigma.sigma.service.ComponentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/components")
+@RequiredArgsConstructor
+public class ComponentController {
+
+    private final ComponentService componentService;
+
+    @GetMapping
+    public ResponseEntity<List<ComponentResponseDTO>> getAllComponents(){
+        return ResponseEntity.ok(componentService.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ComponentResponseDTO> getComponentById(@PathVariable UUID id){
+        return ResponseEntity.ok(componentService.findById(id));
+    }
+
+
+    @PostMapping
+    public  ResponseEntity<ComponentResponseDTO> createComponent(@Valid @RequestBody ComponentRequestDTO dto){
+        ComponentResponseDTO createdComponent = componentService.create(dto);
+        return new ResponseEntity<>(createdComponent, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ComponentResponseDTO> updateComponent(@PathVariable UUID id, @Valid @RequestBody ComponentRequestDTO dto){
+        return ResponseEntity.ok(componentService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComponent(@PathVariable UUID id){
+        componentService.delete(id);
+    }
+}
